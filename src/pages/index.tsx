e3e061fcbbf2d416bf9751c58244d5b9bb54ee31
Daniel Costa <dancosta.id@gmail.com>
2022-04-16 11:39:57 -0300

    created the nextjs app. added some styles to the header

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+          <title>Home | dc.News</title>
+      </Head>
+      <div>
+        <h1>Fala mundo!</h1>
+      </div>
+    </>
+  )
+}
