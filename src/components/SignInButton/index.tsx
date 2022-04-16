405da67b95327f65f884a164dc7eecd115096bd6
Daniel Costa <dancosta.id@gmail.com>
2022-04-16 15:29:10 -0300

    server side handling is working. Added the stripe api and made the connections

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -4,7 +4,7 @@ import styles from './styles.module.scss'
 
 
 export const SignInButton = () => {
-  const isUserLoggedIn = true
+  const isUserLoggedIn = false
 
   return isUserLoggedIn ? (
     <button 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,17 @@
+import { GetServerSideProps } from 'next'
 import Head from 'next/head'
+import { SubscribeButton } from '../components/SubscribeButton'
+import { stripe } from '../services/stripe'
 import styles from './home.module.scss'
 
-export default function Home() {
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  }
+}
+
+export default function Home({product}: HomeProps) {
   return (
     <>
       <Head>
@@ -13,12 +23,32 @@ export default function Home() {
           <h1>My journey with <span>React</span> learning</h1>
           <p>
             Get access to all my publications <br />
-            <span>for $5.99</span>
+            <span>for {product.amount}</span> month.
           </p>
+          <SubscribeButton priceId={product.priceId} />
         </section>
 
         <img src="/images/avatar.svg" alt="boy coding" width="350" />
       </main>
     </>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const price = await stripe.prices.retrieve('price_1KpFb5IegnlGE76yNSor5dKV', {
+    expand: ['product'] // to show name and info of the product
+  })
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+    }).format(price.unit_amount / 100), // receiving the price in cents
+  }
+  return {
+    props:{
+      product
+    }
+  }
 }
