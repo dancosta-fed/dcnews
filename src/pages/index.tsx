c5dba35c295dff097d0b521cf2b014ccbec0214c
Daniel Costa <dancosta.id@gmail.com>
2022-04-16 13:39:02 -0300

    finalizado a página home da aplicação

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import styles from './home.module.scss'
 
 export default function Home() {
   return (
@@ -6,9 +7,18 @@ export default function Home() {
       <Head>
           <title>Home | dc.News</title>
       </Head>
-      <div>
-        <h1>Fala mundo!</h1>
-      </div>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏🏾  Hey, welcome to</span>
+          <h1>My journey with <span>React</span> learning</h1>
+          <p>
+            Get access to all my publications <br />
+            <span>for $5.99</span>
+          </p>
+        </section>
+
+        <img src="/images/avatar.svg" alt="boy coding" width="350" />
+      </main>
     </>
   )
 }
